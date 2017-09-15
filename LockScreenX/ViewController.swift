dd7aee39847b72eefb30f1dab48e47325e6ff944
Malcolm Kumwenda <malcolm@byteorbit.com>
2017-09-15 08:45:16 +0200

    Initial Commit

---
+++ LockScreenX/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LockScreenX
+//
+//  Created by Malcolm Kumwenda on 2017/09/15.
+//  Copyright © 2017 Byte Orbit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
